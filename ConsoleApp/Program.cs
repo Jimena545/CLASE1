dc177fc8cf38f38c3bfcd762446495c59cdc530d
Jimena545 <jimenacuartas05@gmail.com>
2025-08-21 09:41:53 -0500

    primeras clases

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Personas persona = new Personas();
+persona.id = 1;
+persona.nombre = "Maria Rodriguez";
+persona.estatura = 1.60;
+persona.vive = false;
+persona.fecha = datetime.now;
+persona.Estado = new Estados() { id = 1, nombre = "viudo"};
+
+public class Estados
+{
+    public int id;
+    public string? nombre;
+}
+public class Personas
+{
+    public int id;
+    public string? nombre;
+    public int estatura;
+    public bool vive;
+    public datetime fecha;
+    public Estados? Estado;
+}
+
+public class Estudiantes : Personas
+{
+
+    public string? Carnet;
+}
