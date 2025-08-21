29e3d9e89ea31d61743432794e4585768840415e
Jimena545 <jimenacuartas05@gmail.com>
2025-08-21 09:24:52 -0500

    primeras clases

--- ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd42d8a23fbd907182b3f0f24ff0f6a3c285e8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2782ddbdb5c805fc3e658615bc138cece2649adf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
